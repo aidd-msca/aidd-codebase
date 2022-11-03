365a06ee7823979374ac116ca537b3937b6340ef
Peter Hartog <peter.hartog@hotmail.nl>
2022-11-03 14:16:54 +0100

    added new_project descriptions

--- utils/setup.py
+++ utils/setup.py
@@ -5,7 +5,7 @@ from omegaconf import DictConfig, OmegaConf
 from omegaconf.errors import ConfigAttributeError
 
 from aidd_codebase.utils.device import Device
-
+import pytorch_lightning as pl
 
 def setup(cfg: DictConfig, verbose: bool = True) -> Tuple[logging.Logger, Device]:
     logger = logging.getLogger(__name__)
@@ -18,8 +18,15 @@ def setup(cfg: DictConfig, verbose: bool = True) -> Tuple[logging.Logger, Device
     logging.info("Logging is set up")
     logging.debug(OmegaConf.to_yaml(cfg))
     
+    logging.info("setting seed")
+    pl.seed_everything(cfg.seed)
+
     logging.info("setting up device")
-    device = Device(cfg.device)
+    device = Device(
+        device=cfg.device,
+        multi_gpu=cfg.multi_gpu,
+        precision=cfg.precision,
+    )
 
     if verbose:
         print(OmegaConf.to_yaml(cfg))
