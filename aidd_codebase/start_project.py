11891f6a1e7d731b6ccd4a229874629d2319ac52
Peter Hartog <peter.hartog@hotmail.nl>
2022-11-03 15:30:37 +0100

    some refactoring

--- aidd_codebase/start_project.py
+++ aidd_codebase/start_project.py
@@ -1,17 +1,18 @@
-import sys
-import os 
+import os
 import shutil
+import sys
 
-def start(name: str ="project"):
+
+def start(name: str = "project"):
     # Get the current working directory
-    pkgdir = sys.modules['aidd_codebase'].__path__[0]
+    pkgdir = sys.modules["aidd_codebase"].__path__[0]  # type: ignore
     src = os.path.join(pkgdir, "new_project")
     dest = os.path.join(os.getcwd(), name)
-    
+
     # Check if the project directory already exists
     if os.path.exists(dest):
         raise FileExistsError("Project directory already exists")
-    
-    # Copy the content of 
-    # source to destination 
-    shutil.copytree(src, dest) 
+
+    # Copy the content of
+    # source to destination
+    shutil.copytree(src, dest)
