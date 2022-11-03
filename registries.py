1797f797d18f63f17c66245845613df4d318d336
Peter Hartog <peter.hartog@hotmail.nl>
2022-11-03 15:30:37 +0100

    some refactoring

--- registries.py
+++ registries.py
@@ -1,13 +1,13 @@
 from abstract_codebase.registration import RegistryFactory
 
 
-
-
 class ModelRegistry(RegistryFactory):
     pass
 
+
 class MetricRegistry(RegistryFactory):
     pass
+
 
 class DataRegistry(RegistryFactory):
     pass
