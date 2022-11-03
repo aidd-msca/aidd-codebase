9ad81a582fc1ff9bb53efebcbb66f845e684e631
Peter Hartog <peter.hartog@hotmail.nl>
2022-11-03 14:16:54 +0100

    added new_project descriptions

--- aidd_codebase/new_project/main.py
+++ aidd_codebase/new_project/main.py
@@ -2,11 +2,39 @@ import hydra
 from omegaconf import DictConfig
 from aidd_codebase.utils.setup import setup
 from aidd_codebase.utils.config_checker import ConfigChecker
+from abstract_codebase.registration import RegistryFactory
+import pytorch_lightning as pl
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def main(cfg: DictConfig):
     logger, device = setup(cfg)
+    
     # your code here
+    model = None
+    datamodule = None
+    
+    # set up the trainer
+    trainer = pl.Trainer(
+        fast_dev_run=cfg.debug,
+        max_epochs=cfg.epochs,
+        accelerator=device.accelerator,
+        gpus=device.gpus,
+        precision=device.precision,
+        logger=None,
+        log_every_n_steps=1,
+        callbacks=None,
+        weights_save_path=None,
+    )
+
+    # train the model
+    trainer.fit(model=model, datamodule=datamodule)
+
+    # test the model
+    trainer.test(model=model, datamodule=datamodule)
+
+    # get accreditation
+    logger.info("Getting accreditation")
+    RegistryFactory.view_accreditations()
 
 if __name__ == "__main__":
     ConfigChecker()
